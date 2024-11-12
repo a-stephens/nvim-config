3eccf4159e78c98370f2c98b31f09d912f46a09e
Alex Stephens <astephens7000@yahoo.com>
2024-11-11 22:57:29 -0500

    initial commit

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,33 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  lazy = false,
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = 'open_current',
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+      },
+    },
+  },
+}
